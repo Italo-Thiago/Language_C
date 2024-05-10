9bfb4367ca86a5858011aa7c6cd902322d5cb0e2
Italo Thiago <italothiago220@gmail.com>
2024-05-10 12:07:16 -0300

    feat: add C Prograns

---
+++ Output Formatting/Output_Formatting.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Portuguese\n\n");
+    printf("Oi, tudo bem? Tenho 6 anos e programo.\n");
+
+    printf("Valor inteiro: %d.\n", 10);
+    printf("Valor real: %f.\n", 3.14159265);
+    printf("Valor real com apenas duas casas: %.2f.\n", 3.14159265);
+    printf("Dado de texto: %c.\n", 'a');
+    printf("Dado de texto: %s.\n", "testando\n");
+
+    printf("English\n\n");
+    printf("Hi, how are you? I'm 6 years old and I program.\n");
+
+    printf("Integer value: %d.\n", 10);
+    printf("Real value: %f.\n", 3.14159265);
+    printf("Real value with only two places: %.2f.\n", 3.14159265);
+    printf("Text data: %c.\n", 'a');
+    printf("Text data: %s.\n", "testing");
+}
