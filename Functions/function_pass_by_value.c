43cca39c4f72256754eb7244c106de5fcc8403d4
Italo Thiago <italothiago220@gmail.com>
2024-06-02 12:23:02 -0300

    feat: add function pass by value

---
+++ Functions/function_pass_by_value.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int Soma (int x, int y)
+{
+    int z = x + y;
+
+    puts("==> Função");
+    printf("&x = %p, X = %d\n", &x, x);
+    printf("&y = %p, Y = %d\n", &y, y);
+    printf("&z = %p, Z = %d\n\n", &z, z);
+
+    return z;
+}
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c;
+
+    puts("==> Antes de Chamar a Função");
+    printf("&a = %p, A = %d\n", &a, a);
+    printf("&b = %p, B = %d\n", &b, b);
+    printf("&c = %p, C = %d\n\n", &c, c);
+
+    c = Soma (a, b);
+
+    puts("==> Depois de Chamar a Função");
+    printf("&a = %p, X = %d\n", &a, a);
+    printf("&b = %p, Y = %d\n", &b, b);
+    printf("&c = %p, Z = %d\n\n", &c, c);
+
+    return 0;
+}
+
